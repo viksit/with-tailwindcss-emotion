e6a21a80d0f8e7714abbf1348d127b0e37446a95
viksit <viksit@gmail.com>
2021-04-09 21:34:00 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  variants: {},
+  plugins: [],
+  // xwind options
+  xwind: {
+    mode: 'objectstyles',
+  },
+}
